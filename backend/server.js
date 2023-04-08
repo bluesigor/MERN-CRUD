11c1a5546eb299d35a864d8f8068afc8e908a121
Andrian Sarabryn <andrianupwork@gmail.com>
2023-04-08 14:47:25 +0300

    feat/request

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log('server');
+
+const express = require('express');
+const formData = require('express-form-data');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+
+const app = express();
+const port = process.env.PORT || 6000;
+
+app.use(formData.parse());
+
+app.use('/api/goals', require('./routes/goalRoutes'));
+app.use(errorHandler);
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
