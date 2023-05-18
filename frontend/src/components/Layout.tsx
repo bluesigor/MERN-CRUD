ee830e9b3c22474ba514deaf24f57c7839eb880a
Andrian Sarabryn <andrianupwork@gmail.com>
2023-05-18 13:08:17 +0300

    and

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import { Outlet } from "react-router-dom";
 import Header from "./Header";
 
 const Layout = ({ children }: React.Element) => {
+  const [state, setState] = useState();
   return (
     <>
       <Header />
